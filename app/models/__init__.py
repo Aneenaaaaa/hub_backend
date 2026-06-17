db45e62e65e341a31eed813ae6c1771baa0a212d
iamjki <143060254+iamjki@users.noreply.github.com>
2026-06-17 11:25:17 +0530

    Add NotificationJob model and notification API endpoints (#23)
    * Create README.md
    
    * feature(notifications): add NotificationJob model and notification APIs
    
    * env correction
    
    ---------

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,14 @@ from app.models.chat import ChatSession, ChatMessage
 from app.models.document import Document
 from app.models.todo import Todo
 from app.models.poll import PollResponse
-from app.models.notification_job import NotificationJob
+from app.models.notification import NotificationJob
 
-__all__ = ["User", "ChatSession", "ChatMessage", "Document", "Todo", "PollResponse", "NotificationJob"]
+all = [
+"User",
+"ChatSession",
+"ChatMessage",
+"Document",
+"Todo",
+"PollResponse",
+"NotificationJob",
+]
